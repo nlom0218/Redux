2e0e7ce002975a10e1715dfae06d3886046b57e4
KIM HONG DONG <nlom0218@naver.com>
2021-03-26 22:29:26 +0900

    finish~!
    finish~!

--- src/web.js
+++ src/web.js
@@ -23,7 +23,7 @@ const TOC = () => {
     contents.forEach(content => {
         document.getElementById(`${content.id}`).addEventListener("click", (e) => {
             e.preventDefault()
-            store.dispatch({ type: "SELETE", id: content.id })
+            store.dispatch({ type: "SELETE", id: content.id, mode: "read" })
         })
     });
 };
@@ -32,32 +32,68 @@ const TOC = () => {
 const control = () => {
     document.getElementById("control").innerHTML = `
     <ul>
-        <li><a href="/create">Create</a></li>
-        <li><input type="button" value="Delete"></li>
+        <li id="jsCreate"><a href="/create">Create</a></li>
+        <li><input type="button" value="Delete" id="delBtn"></li>
     </ul>
-`
+    `
+    document.getElementById("jsCreate").addEventListener("click", (e) => {
+        e.preventDefault()
+        store.dispatch({ type: "CREATE", mode: "create" })
+    })
+    document.getElementById("delBtn").addEventListener("click", (e) => {
+        e.preventDefault()
+        const deleteContent = () => {
+            const { content_id, contents } = store.getState()
+            console.log(content_id);
+            return contents.filter(content => content.id !== content_id)
+        }
+        console.log(deleteContent())
+        store.dispatch({ type: "DELETE", contents: deleteContent(), mode: "home" })
+    })
 }
 
 const article = async () => {
-    const { content_id, contents } = store.getState();
-    const content = await contents.filter(item => item.id === content_id);
-    try {
-
-        // console.log(content);
-        // console.log(content[0]);
-        console.log(content[0].id);
+    const { content_id, contents, mode } = store.getState();
+    if (mode === "read") {
+        try {
+            const content = await contents.filter(item => item.id === content_id);
+            document.getElementById("article").innerHTML = `
+            <article>
+                <h2>${content[0].title}</h2>
+                ${content[0].desc}
+            </article>
+            `
+        } catch (err) {
+            console.log(err);
+        }
+    } else if (mode === "create") {
         document.getElementById("article").innerHTML = `
         <article>
-        <h2>${content[0].title}</h2>
-        ${content[0].desc}
-         </article>
+            <form id="jsForm">
+                <p><input type="text" name="title" placeholder="title"</p>
+                <p><textarea name="desc" placeholder="desc"></textarea></p>
+                <p><button type="submit">Submit</button></p>
+            </form>
+        </article>
         `
-    } catch (err) {
-        console.log(err);
+        document.getElementById("jsForm").addEventListener("submit", (e) => {
+            e.preventDefault()
+            const { title: { value: title } } = e.target
+            const { desc: { value: desc } } = e.target
+            store.dispatch({ type: "UPDATE", newContent: { id: Date.now(), title, desc }, id: Date.now(), mode: "read" })
+        })
+    } else if (mode === "home") {
+        document.getElementById("article").innerHTML = `
+        <article>
+            <h2>Welcome Redux~!</h2>
+            Hello, Everyone
+        </article>
+        `
     }
 }
 
 const initialState = {
+    mode: "home",
     content_id: 0,
     contents: [
         { id: 1, title: "HTML", desc: "HTML is ..." },
@@ -66,10 +102,16 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    // console.log(state, action);
+    console.log(state, action);
     switch (action.type) {
         case "SELETE":
-            return { ...state, ...{ content_id: action.id } }
+            return { ...state, content_id: action.id, mode: action.mode }
+        case "UPDATE":
+            return { contents: [...state.contents, action.newContent], content_id: action.id, mode: action.mode }
+        case "DELETE":
+            return { ...state, contents: action.contents, mode: action.mode }
+        case "CREATE":
+            return { ...state, mode: action.mode }
         default:
             return state
     }
@@ -86,4 +128,5 @@ const init = () => {
 }
 
 store.subscribe(article)
+store.subscribe(TOC)
 init()
